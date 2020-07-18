31268a1f5e5f745783f5507bc758cab923dd301b
lcrocket <liamcrocket@gmail.com>
2020-07-18 14:30:39 -0400

    Added login page header

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import Button from "../components/Button";
+import Logo from "../assets/train-track-init-logo.svg";
+import "../styles/LogIn.css";
 
 const LogIn: React.FC = () => {
-  return <Button text="Sign Up" size="large" href="#" />;
+  return (
+    <div className="login-container">
+      <div className="login-header">
+        <img src={Logo} alt="Train Track Logo" className="train-track-logo" />
+        <Button text="Sign Up" size="medium" href="#" />
+      </div>
+    </div>
+  );
 };
 
 export default LogIn;
