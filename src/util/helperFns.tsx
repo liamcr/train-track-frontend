7c2e407b627e413869c10ac2cfc0baa7fde5720a
lcrocket <liamcrocket@gmail.com>
2020-08-15 12:41:23 -0400

    Added documentation for setAccessToken

--- src/util/helperFns.tsx
+++ src/util/helperFns.tsx
@@ -1,3 +1,8 @@
+/**
+ * Handles the updating and verification of the user's access token.
+ * Redirects the user to the log-in page if there is no access token
+ * available.
+ */
 export const setAccessToken = () => {
   const windowHash = window.location.hash.substr(1);
 
