4b36424a010761d9474ce9de774bf114b175e0d5
lcrocket <liamcrocket@gmail.com>
2020-12-24 14:55:50 -0500

    Added profile icon for users without profile image

--- src/components/ProfileImage.tsx
+++ src/components/ProfileImage.tsx
@@ -1,4 +1,5 @@
 import { Typography } from "@material-ui/core";
+import { Person } from "@material-ui/icons";
 import React from "react";
 import "../styles/ProfileImage.css";
 import { FullUser } from "../util/commonTypes";
@@ -12,11 +13,15 @@ type ProfileImageProps = {
 const ProfileImage: React.FC<ProfileImageProps> = ({ user }) => {
   return (
     <div className="profile-image-container">
-      <img
-        className="profile-image"
-        src={user?.displayImage}
-        alt={`${user?.username} DP`}
-      />
+      {user && user.displayImage !== "" ? (
+        <img
+          className="profile-image"
+          src={user.displayImage}
+          alt={`${user.username} DP`}
+        />
+      ) : (
+        <Person style={{ width: "100%", height: "100%" }} color="primary" />
+      )}
       <div className="profile-image-overlay">
         <div className="profile-overlay-content">
           <Typography variant="h4">{user?.username}</Typography>
